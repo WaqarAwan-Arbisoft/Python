b3a49512d381f245d7769a19dd3e28e86fc8f0bf
waqar-awan376 <tr.awan.waqar@gmail.com>
2022-07-26 22:42:01 +0500

    Total 20 questions added

--- day1.py
+++ day1.py
@@ -58,3 +58,106 @@ print(3 * 'un' + 'ium')
 print('py' 'thon')
 # Output: python
 # Reason: Implicitly the string gets concatenated
+
+
+# ***** Question-11 *****
+print(sum(range(0, 7)))
+# Output: 21
+# Reason: Range returns a number range from 0(inclusive) to 7(exclusive). Then sum it up from sum function which is 21
+
+
+# ***** Question-12 *****
+q12Cubes = [1, 8, 27]
+q12Cubes.append(4 ** 3)
+print(q12Cubes)
+# Output: [1, 8, 27, 64]
+# Reason: Double asterisks means power. 4 power 3 is 64. It is appended to the array
+
+
+# ***** Question-13 *****
+q13Word = "galaxy"
+print(q13Word[4:50])
+# Output: xy
+# Reason: Start from the starting index(4) towards 50th index. If 50 letters are not available then the list will be returns till end.
+
+
+# ***** Question-14 *****
+print(51 % 3)
+# Output: 0
+# Reason: It is modulus operator and returns the remaining after the division. 51 completely gets divided by 3 so it returns zero.
+
+# ***** Question-15 *****
+
+
+def if_confusion(x, y):
+    if x > y:
+        if x - 5 > 0:
+            x = y
+            return "A" if y == y + y else "B"
+        elif x + y > 0:
+            while x > y:
+                x -= 1
+            while y > x:
+                y -= 1
+            if x == y:
+                return "E"
+    else:
+        if x - 2 > y - 4:
+            x_old = x
+            x = y * y
+            y = 2 * x_old
+            if (x - 4) ** 2 > (y - 7) ** 2:
+                return "C"
+            return "D"
+        return "H"
+
+
+print(if_confusion(3, 7))
+# Output: H
+# Reason: Dry run the code and follow the if and loop branchings
+
+# ***** Question-16 *****
+q16Str = 'cool'
+print(q16Str[-1] + q16Str[-2]
+      + q16Str[-4] + q16Str[-3])
+# Output: loco
+# Reason: The negative index gets the last element. -1 defines the last element from end.
+
+
+# ***** Question-17 *****
+q17Words = ['cat', 'mouse']
+for word in q17Words:
+    print(len(q17Words))
+# Output: 3 5
+# Reason: in operator gets the value of each element and assign to word(within the loop) and len operator gets the length of the string
+
+# ***** Question-18 *****
+
+
+def func(x):
+    return x + 1
+
+
+f = func
+print(f(2) + func(2))
+# Output: 6
+# Reason: Line number 141 assigns the function func reference to f. Now we can call the same function with the name f
+
+
+# ***** Question-19 *****
+q19Word = "galaxy"
+print(q19Word[:-2] + q19Word[-2:])
+# Output: galaxy
+# Reason: [:-2] means that start from 0th index towards -2 index which makes gala and [-2:] means start from -2 to end. Now concatenate them which makes galaxy
+
+
+# ***** Question-20 *****
+def q20Func(a, *args):
+    print(a)
+    for arg in args:
+        print(arg)
+
+
+q20Func("A", "B", "C")
+# Output: A B C
+# Reason: a simply gets "A" and *args means that it becomes a list with elements "B" and "C" which can further be iterated and printed.
